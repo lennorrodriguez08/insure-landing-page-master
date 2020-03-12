2ba6a969f24eb270cba1874a064feff5216f464b
lennorrodriguez08 <41136127+lennorrodriguez08@users.noreply.github.com>
2020-03-13 02:59:53 +0800

    Add files via upload
    initial commit

---
+++ dist/script.js
@@ -0,0 +1,32 @@
+// document.addEventListener('DOMContentLoaded', function() {
+
+//    var toggle = document.querySelector('#main-nav .nav-content label .fa-bars');
+//    var exit = document.querySelector('#main-nav .nav-content .fa-times');
+//    var navItem = document.querySelector('#main-nav .nav-content ul');
+
+
+//    toggle.addEventListener('change', function(e){
+
+//       if (toggle.checked) {
+//          toggle.style.display = 'none';
+//       }
+//    })
+
+
+
+
+
+//    toggle.addEventListener('click', function(e) {
+//       toggle.style.display = 'none';
+//       navItem.style.display = 'block';
+//       exit.style.display = 'block';
+//    });
+
+//    exit.addEventListener('click', function(e) {
+//       exit.style.display = 'none';
+//       toggle.style.display = 'block';
+//       navItem.style.display = 'none';
+//    });
+// console.log(toggle);
+
+// })
